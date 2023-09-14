573765013f67830005d0098ca2c7402bf1bc83c4
deequoique <puppy_road@163.com>
2023-09-14 13:45:39 +0800

    first commit

---
+++ app/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <HomeVue/>
+  </div>
+</template>
+
+<script>
+import HomeVue  from './components/HomeVue.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HomeVue
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
